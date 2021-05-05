38844776812be43d7251c50628ffe8977719ef8d
fendriz <torgeirhau@gmail.com>
2021-05-05 19:19:42 +0200

    modifing footer

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,30 +1,29 @@
 import React from "react";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <div className="twitter">
-                <a href="twitter.com" target="_blank">
-                    <i class="fab fa-twitter"></i>
-                </a>
-            </div>
-            <div className="facebook">
-                <a href="facebook.com" target="_blank">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-            </div>
-            <div className="instagram">
-                <a href="instagram.com" target="_blank">
-                    <i class="fab fa-instagram"></i>
-                </a>
-            </div>
-            <div className="mail">
-                <a href="" target="_blank">
-                    <i class="fas fa-envelope"></i>
-                </a>
-            </div>
-
+  return (
+    <div className="footer">
+      <a href="https://witter.com" target="_blank">
+        <div className="twitter">
+          <i class="fab fa-twitter"></i>
         </div>
-    );
+      </a>
+      <a href="https://facebook.com" target="_blank">
+        <div className="facebook">
+          <i class="fab fa-facebook-f"></i>
+        </div>
+      </a>
+      <a href="https://instagram.com" target="_blank">
+        <div className="instagram">
+          <i class="fab fa-instagram"></i>
+        </div>
+      </a>
+      <a href="" target="_blank">
+        <div className="mail">
+          <i class="fas fa-envelope"></i>
+        </div>
+      </a>
+    </div>
+  );
 }
 export default Footer;
