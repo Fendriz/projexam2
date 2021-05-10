fa3f98737c86e09507d07cb70418a33c9814a99f
fendriz <torgeirhau@gmail.com>
2021-05-10 13:21:39 +0200

    fix post for adding hotels

--- src/components/admin/hotels/CreateHotel.js
+++ src/components/admin/hotels/CreateHotel.js
@@ -31,45 +31,79 @@ function CreateHotel() {
 
     await fetch(url, options);
 
-    history.push("/admin/hotels");
+    history.push("/admin/hotels/update");
   }
 
   return (
     <div className="container_hotel">
       <AdminMenu active={1}></AdminMenu>
-      <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form onSubmit={handleSubmit(onSubmit)} className="form_update">
         <AdminHotelMenu active={1}></AdminHotelMenu>
         <Form.Group>
-          <Form.Label>First Name</Form.Label>
+          <Form.Label>Hotel Name</Form.Label>
+          {console.log("test")}
           <Form.Control
-            label="firstName"
-            name="firstName"
-            placeholder="firstName"
-            {...register("firstName")}
+            label="hotel"
+            name="hotel"
+            {...register("hotel")}
+            placeholder="hotel Name"
           />
-          {errors.firstName && (
-            <p class="text-danger">{errors.firstName.message}</p>
-          )}
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            name="lastName"
-            placeholder="lastName"
-            {...register("lastName")}
-          />
-          {errors.lastName && (
-            <p class="text-danger">{errors.lastName.message}</p>
-          )}
+          {errors.hotel && <p class="text-danger">{errors.hotel.message}</p>}
         </Form.Group>
         <Form.Group>
           <Form.Label>Email</Form.Label>
           <Form.Control
             name="email"
-            placeholder="email"
+            placeholder="Email"
             {...register("email")}
           />
           {errors.email && <p class="text-danger">{errors.email.message}</p>}
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Price per Night</Form.Label>
+          <Form.Control
+            name="price"
+            placeholder="heisann"
+            {...register("price")}
+          />
+          {errors.price && <p class="text-danger">{errors.price.message}</p>}
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Max Guests</Form.Label>
+          <Form.Control
+            name="guests"
+            placeholder="Max Guests"
+            {...register("guests")}
+          />
+          {errors.guests && <p class="text-danger">{errors.guests.message}</p>}
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Image Url</Form.Label>
+          <Form.Control name="url" placeholder="heisann" {...register("url")} />
+          {errors.url && <p class="text-danger">{errors.url.message}</p>}
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Latitude</Form.Label>
+          <Form.Control
+            name="lat"
+            placeholder="Latitude"
+            {...register("lat")}
+          />
+          {errors.lat && <p class="text-danger">{errors.lat.message}</p>}
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Longitude</Form.Label>
+          <Form.Control
+            name="lng"
+            placeholder="longitude"
+            {...register("lng")}
+          />
+          {errors.lng && <p class="text-danger">{errors.lng.message}</p>}
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>ID</Form.Label>
+          <Form.Control name="id" placeholder="hotelID" {...register("id")} />
+          {errors.id && <p class="text-danger">{errors.id.message}</p>}
         </Form.Group>
         <Form.Group>
           <Form.Label>Message</Form.Label>
@@ -77,7 +111,7 @@ function CreateHotel() {
             as="textarea"
             rows={6}
             name="mess"
-            placeholder="message"
+            placeholder="Type your message min 10 characters"
             {...register("mess")}
           />
           {errors.mess && <p class="text-danger">{errors.mess.message}</p>}
--- src/components/routes/ProtectedRoute.js
+++ src/components/routes/ProtectedRoute.js
@@ -3,9 +3,16 @@ import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-    const { islogged } = useContext(AuthContext);
+  const { islogged } = useContext(AuthContext);
 
-    return <Route {...rest} render={(props) => (islogged ? <Component {...rest} {...props} /> : <Redirect to="/login" />)} />;
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        islogged ? <Component {...rest} {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
 };
 
 export default ProtectedRoute;
