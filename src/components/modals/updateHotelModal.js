79c845437879839255bb5a5d220bd1908e2681a1
fendriz <torgeirhau@gmail.com>
2021-05-14 23:06:11 +0200

    Adding styling to modal for update hotel

--- src/components/modals/updateHotelModal.js
+++ src/components/modals/updateHotelModal.js
@@ -23,8 +23,9 @@ function UpdateHotelModal(props) {
   } = useForm({
     resolver: yupResolver(SchemaEnquire),
   });
-  async function onSubmit(data) {
-      const fetchUrl = BASE_URL + "establishments/" + props.id;
+  async function onSubmit() {
+  console.log(props)
+      const fetchUrl = BASE_URL + "establishments/" + props.hotel.id;
       const updateOptions = {
         headers,
         method: PATCH,
@@ -32,106 +33,124 @@ function UpdateHotelModal(props) {
       };
       await fetch(fetchUrl, updateOptions);
     }
-  console.log(props.data)
-  console.log(props.hotel)
   return (
       <Modal
         show={ismodal}
         onHide={closeModal}
         backdrop="static"
         keyboard={false}
-        className="enquire-modal"
+        id="update-modal"
       >
         <Modal.Header closeButton className="enquire-modal-header">
-          <Modal.Title>Enquire Hotel</Modal.Title>
+          <Modal.Title>Check changes before update</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="formContainer">
-            <p>We will get back to you as soon as possible </p>
             <Form onSubmit={handleSubmit(onSubmit)} className="form_update" id="form_modal-update">
-              <Form.Group className="form-group-flex ">
+              <Form.Group className="form-group-update ">
                 <Form.Group className="form-group-left ">
-                  <Form.Group className="modal-old">
-                    <Form.Label>Old</Form.Label>
-                    <Form.Control readOnly value={props.hotel.name} />
+                  <Form.Group className="modal-group">
+                    <Form.Group className="modal-old">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control name="name" {...register("name")} readOnly isInvalid value={props.hotel.name} />
+                    </Form.Group>
+                    <Form.Group className="modal-new">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control name="hotel" {...register("name")} readOnly isValid value={props.data.name} />
+                    </Form.Group>
                   </Form.Group>
-                  <Form.Group className="modal-new">
-                    <Form.Label>New</Form.Label>
-                    <Form.Control readOnly value={props.data.name} />
+                  <Form.Group className="modal-group">
+                    <Form.Group className="modal-old">
+                      <Form.Label>address</Form.Label>
+                      <Form.Control readOnly isInvalid value={props.hotel.address} />
+                    </Form.Group>
+                    <Form.Group className="modal-new">
+                      <Form.Label>address</Form.Label>
+                      <Form.Control readOnly isValid value={props.data.address} />
+                    </Form.Group>
                   </Form.Group>
-                  <Form.Group className="modal-old">
-                    <Form.Label>Old</Form.Label>
-                    <Form.Control readOnly value={props.hotel.address} />
+                  <Form.Group className="modal-group">
+                    <Form.Group className="modal-old">
+                      <Form.Label>email</Form.Label>
+                      <Form.Control readOnly isInvalid value={props.hotel.email} />
+                    </Form.Group>
+                    <Form.Group className="modal-new">
+                      <Form.Label>email</Form.Label>
+                      <Form.Control readOnly isValid value={props.data.email} />
+                    </Form.Group>
                   </Form.Group>
-                  <Form.Group className="modal-new">
-                    <Form.Label>New</Form.Label>
-                    <Form.Control readOnly value={props.data.address} />
+                  <Form.Group className="modal-group">
+                    <Form.Group className="modal-old">
+                      <Form.Label>price</Form.Label>
+                      <Form.Control readOnly isInvalid value={props.hotel.price} />
+                    </Form.Group>
+                    <Form.Group className="modal-new">
+                      <Form.Label>price</Form.Label>
+                      <Form.Control readOnly isValid value={props.data.price} />
+                    </Form.Group>
                   </Form.Group>
-                  <Form.Group className="modal-old">
-                    <Form.Label>Old</Form.Label>
-                    <Form.Control readOnly value={props.hotel.email} />
+                  <Form.Group className="modal-group">
+                    <Form.Group className="modal-old">
+                      <Form.Label>maxGuests</Form.Label>
+                      <Form.Control readOnly isInvalid value={props.hotel.maxGuests} />
+                    </Form.Group>
+                    <Form.Group className="modal-new">
+                      <Form.Label>maxGuests</Form.Label>
+                      <Form.Control readOnly isValid value={props.data.maxGuests} />
+                    </Form.Group>
                   </Form.Group>
-                  <Form.Group className="modal-new">
-                    <Form.Label>New</Form.Label>
-                    <Form.Control readOnly value={props.data.email} />
+                  <Form.Group className="modal-group">
+                    <Form.Group className="modal-old">
+                      <Form.Label>lat</Form.Label>
+                      <Form.Control readOnly isInvalid value={props.hotel.lat} />
+                    </Form.Group>
+                    <Form.Group className="modal-new">
+                      <Form.Label>lat</Form.Label>
+                      <Form.Control readOnly isValid value={props.data.lat} />
+                    </Form.Group>
                   </Form.Group>
-                  <Form.Group className="modal-old">
-                    <Form.Label>Old</Form.Label>
-                    <Form.Control readOnly value={props.hotel.price} />
-                  </Form.Group>
-                  <Form.Group className="modal-new">
-                    <Form.Label>New</Form.Label>
-                    <Form.Control readOnly value={props.data.price} />
-                  </Form.Group>
-                  <Form.Group className="modal-old">
-                    <Form.Label>Old</Form.Label>
-                    <Form.Control readOnly value={props.hotel.maxGuests} />
-                  </Form.Group>
-                  <Form.Group className="modal-new">
-                    <Form.Label>New</Form.Label>
-                    <Form.Control readOnly value={props.data.maxGuests} />
-                  </Form.Group>
-                  <Form.Group className="modal-old">
-                    <Form.Label>Old</Form.Label>
-                    <Form.Control readOnly value={props.hotel.lat} />
-                  </Form.Group>
-                  <Form.Group className="modal-new">
-                    <Form.Label>New</Form.Label>
-                    <Form.Control readOnly value={props.data.lat} />
-                  </Form.Group>
-                  <Form.Group className="modal-old">
-                    <Form.Label>Old</Form.Label>
-                    <Form.Control readOnly value={props.hotel.lng} />
-                  </Form.Group>
-                  <Form.Group className="modal-new">
-                    <Form.Label>New</Form.Label>
-                    <Form.Control readOnly value={props.data.lng} />
+                  <Form.Group className="modal-group">
+                    <Form.Group className="modal-old">
+                      <Form.Label>lng</Form.Label>
+                      <Form.Control readOnly isInvalid value={props.hotel.lng} />
+                    </Form.Group>
+                    <Form.Group className="modal-new">
+                      <Form.Label>lng</Form.Label>
+                      <Form.Control readOnly isValid value={props.data.lng} />
+                    </Form.Group>
                   </Form.Group>
                 </Form.Group>
                 <Form.Group className="form-group-right form-margintop">
-                  <Form.Group>
-                    <Form.Label>New</Form.Label>
-                    <Form.Control readOnly value={props.data.image} />
+                  <Form.Group> 
+                    <Form.Group className="modal-old">
+                      <Form.Label>image</Form.Label>
+                      <Form.Control readOnly isInvalid value={props.hotel.image} />
+                    </Form.Group>
+                    <Form.Group className="modal-new">
+                      <Form.Label>image</Form.Label>
+                      <Form.Control readOnly isValid value={props.data.image} />
+                    </Form.Group>
                   </Form.Group>
                   <Form.Group>
-                    <Form.Label>Old</Form.Label>
-                    <Form.Control readOnly value={props.hotel.price} />
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>Old</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      defaultValue={props.data.description}
-                      readOnly
-                    />
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>New</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      defaultValue={props.hotel.description}
-                      readOnly
-                    />
+                    <Form.Group >
+                      <Form.Control
+                        as="textarea"
+                        defaultValue={props.hotel.description}
+                        readOnly
+                        className="modal-old"
+                        isInvalid 
+                        textarea="5"
+                      />
+                    </Form.Group>
+                    <Form.Group>
+                      <Form.Control
+                        as="textarea"
+                        defaultValue={props.data.description}
+                        readOnly
+                        className="modal-old"
+                        isValid  
+                      />
+                    </Form.Group>
                   </Form.Group>
                   <Form.Group>
                     <Form.Label>Old</Form.Label>
